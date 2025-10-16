f70b1e5a494b57975747fa8b7de28c548a687a4a
Salk773 <saalk773@gmail.com>
2025-10-16 16:19:47 +0100

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <main style={{minHeight:'100vh',display:'grid',placeItems:'center',fontFamily:'system-ui'}}>
+      <div style={{textAlign:'center'}}>
+        <h1 style={{fontSize:48, marginBottom:12}}>Printly</h1>
+        <p style={{opacity:.8}}>Made layer by layer.</p>
+        <p style={{marginTop:24}}>Store is spinning up. Check back soon 👋</p>
+      </div>
+    </main>
+  );
+}
