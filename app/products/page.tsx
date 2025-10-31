07f6390f56a1dd3cad35d36aeca0c59c0408ffdf
Salk773 <saalk773@gmail.com>
2025-10-31 14:02:48 +0000

    Update page.tsx

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,4 +1,4 @@
-import { supabase } from "@/lib/supabaseServer"
+import { supabaseServer } from "../../lib/supabaseServer";
 import Link from "next/link"
 
 export default async function ProductsPage() {
