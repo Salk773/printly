414d70541b2295010b87a0e4747bfb0a1e2b773e
Salk773 <saalk773@gmail.com>
2025-11-04 10:45:44 +0000

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer className="footer">
-      <div className="container" style={{textAlign:'center'}}>
+      <div className="container" style={{ textAlign: "center" }}>
         © {new Date().getFullYear()} Printly. Made in the UAE 🇦🇪
       </div>
     </footer>
