79b6ce6b6d858bf2b4d0af8e7c9775afbb0878a6
Salk773 <saalk773@gmail.com>
2025-11-04 10:46:03 +0000

    Update supabaseServer.ts

--- lib/supabaseServer.ts
+++ lib/supabaseServer.ts
@@ -1,11 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
-// ✅ Creates a Supabase client for server components
 export const supabaseServer = () =>
   createClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-    {
-      auth: { persistSession: false },
-    }
+    { auth: { persistSession: false } }
   );
