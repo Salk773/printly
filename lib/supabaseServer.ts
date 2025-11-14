8696fffc16d4b1cd1b2139c740a88ac9a89d45ea
Salk773 <saalk773@gmail.com>
2025-11-14 12:57:26 +0000

    Update supabaseServer.ts

--- lib/supabaseServer.ts
+++ lib/supabaseServer.ts
@@ -1,8 +1,8 @@
 // lib/supabaseServer.ts
 import { createClient } from "@supabase/supabase-js";
 
-// This client is SAFE for server-side read operations
-// It uses the public anon key and respects RLS.
+// Safe, server-side client using the public anon key.
+// Respects RLS - used for normal site (products, categories).
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
