6e8cda8d0397ae4e893068561159bbe0d7ae4737
Salk773 <saalk773@gmail.com>
2025-11-25 11:07:15 +0000

    Update supabaseAdmin.ts

--- lib/supabaseAdmin.ts
+++ lib/supabaseAdmin.ts
@@ -4,8 +4,7 @@ import { createClient } from "@supabase/supabase-js";
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
 const serviceRoleKey = process.env.SUPABASE_SERVICE_ROLE_KEY!;
 
-export function supabaseAdmin() {
-  return createClient(supabaseUrl, serviceRoleKey, {
-    auth: { persistSession: false },
+export const supabaseAdmin = () =>
+  createClient(supabaseUrl, serviceRoleKey, {
+    auth: { persistSession: false }
   });
-}
