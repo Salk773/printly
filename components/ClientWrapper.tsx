28347c41de55f9dc53ffbe8919861a8d4a2541cd
Salk773 <saalk773@gmail.com>
2025-12-02 10:23:42 +0000

    Update ClientWrapper.tsx

--- components/ClientWrapper.tsx
+++ components/ClientWrapper.tsx
@@ -1,23 +1,17 @@
 "use client";
 
 import { CartProvider } from "@/context/CartContext";
-import Navbar from "./Navbar";
-import Footer from "./Footer";
 import { Toaster } from "react-hot-toast";
 
 export default function ClientWrapper({
-  children
+  children,
 }: {
   children: React.ReactNode;
 }) {
   return (
     <CartProvider>
-      <div className="app-shell">
-        <Navbar />
-        <main className="page">{children}</main>
-        <Footer />
-      </div>
-      <Toaster position="bottom-center" />
+      {children}
+      <Toaster position="bottom-right" />
     </CartProvider>
   );
 }
