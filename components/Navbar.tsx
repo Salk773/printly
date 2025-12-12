126c740227e4095c4e90d8b19967a10486661c02
Salk773 <saalk773@gmail.com>
2025-12-12 10:17:01 +0000

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Link from "next/link";
-import { useCart } from "@/context/CartContext";
+import { useCart } from "@/context/CartProvider";   // ✅ FIXED IMPORT
 
 export default function Navbar() {
   const { count } = useCart();
